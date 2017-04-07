6d34c95125cec24ef51d055800edd1ef07d9b3b0
两好三坏 <suqiankun@print.local>
2017-04-07 15:54:02 +0800

    抽取了获取社区的函数

--- js/Common.js
+++ js/Common.js
@@ -1,17 +1,33 @@
 var common = {
-	
-login: function loginActive(account, psw) {
-		//判断密码&用户名
-		if(account == 'admins' & psw == '123456') {
-			mui.openWindow({
-				url: 'tab-webview-main.html',
-				id: 'index'
-			});
-		} else {
-			mui.alert(' ', '账号或密码错误!', function() {});
-		}
-		return "12345";
+getSheQu: function getSheQu( callback) {
+//获取社区列表
+			var resultStr = ''; 
+			mui.ajax('http://117.146.77.26:8080/app?cmd=getcommunitylist',{
+				dataType:'json',
+				type:'get',
+				success:function(data){
+				var area_name ='<option value="">请选择</option>';
+				for (i=0;i<data.data.length;i++) {
 
+					if (data.data.length  > 1) {
+						area_name +=
+						'<option value="'+
+						data.data[i].area_id	
+						+'">'+ data.data[i].area_name+'</option>';
+					} else{
+						
+						area_name +=
+						'<option value="'+
+						data.data[i].area_id
+						+'" selected = selected>'+ data.data[i].area_name+'</option>';
+					}
+				}
+				resultStr = area_name;
+				callback(resultStr);		
+				},
+				error: function(xhr,type,errorThrown){
+				}
+			});
 	},
 	
 	getAjax: function getAjax(action){
@@ -27,23 +43,20 @@ login: function loginActive(account, psw) {
 				}
 			});
 			return userdata;
-		
 	},
-	
-	
 	//获取type信息;
 	getType: function getType(cmd,actionid,params) {
+		
 		var areaStr = '';
 		//获取宗教信仰列表
-			  mui.ajax('http://124.117.230.75:8080/app?cmd=getdictbytype&dictype='+cmd,{
+			  mui.ajax('http://1117.146.77.26:8080/app?cmd=getdictbytype&dictype='+cmd,{
 				dataType:'json',
 				type:'get',
 				success:function(data){
 				area_name = '<option value="">请选择</option>';
-				for (i=0;i<data.data.length;i++) {		
-					
+			
+				for (i=0;i<data.data.length;i++) {
 					area_name +='<option value="'+data.data[i].ids+'" >'+ data.data[i].val+'</option>';
-							
 				}
 				areaStr = area_name;
 				document.getElementById(actionid).innerHTML = area_name;
@@ -53,6 +66,74 @@ login: function loginActive(account, psw) {
 				});
 				
 				return areaStr;
+	},
+	//获取兵役设置初始化信息;
+	getBingyi: function getBingyi(cmd,actionid,params) {
+		//获取兵役状况
+			  mui.ajax('http://117.146.77.26:8080/app?cmd=getdictbytype&dictype='+cmd,{
+				dataType:'json',
+				type:'get',
+				success:function(data){
+				area_name = '<option value="">请选择</option>';
+			
+				for (i=0;i<data.data.length;i++) {
+						if(params == data.data[i].val || params == data.data[i].ids  ) {
+						
+						if(params == data.data[i].val   ) {
+							area_name +='<option value="'+data.data[i].ids+'" selected = selected>'+ data.data[i].val+'</option>';
+						}
+					
+						if(params == data.data[i].ids ) {
+							area_name +='<option value="'+data.data[i].ids+'" selected = selected>'+ data.data[i].val+'</option>';
+						}
+						
+					}else{
+						area_name +='<option value="'+data.data[i].ids+'">'+ data.data[i].val+'</option>';
+					}					
+				}
+				document.getElementById(actionid).innerHTML = area_name;
+				},
+				error: function(xhr,type,errorThrown){
+					
+				}
+				});
+				
+	},
+	//获初始化信息;
+	getReligion: function getReligion(cmd,actionid,params) {
+		
+			  mui.ajax('http://117.146.77.26:8080/app?cmd=getdictbytype&dictype='+cmd,{
+				dataType:'json',
+				type:'get',
+				success:function(data){
+				area_name = '<option value="">请选择</option>';
+			
+				for (i=0;i<data.data.length;i++) {
+					
+					if(params == data.data[i].val || params == data.data[i].ids  ) {
+						
+						if(params == data.data[i].val   ) {
+							area_name +='<option value="'+data.data[i].ids+'" selected = selected>'+ data.data[i].val+'</option>';
+						}
+					
+						if(params == data.data[i].ids ) {
+							area_name +='<option value="'+data.data[i].ids+'" selected = selected>'+ data.data[i].val+'</option>';
+						}
+						
+					}else{
+						area_name +='<option value="'+data.data[i].ids+'">'+ data.data[i].val+'</option>';
+					}
+					
+					
+//					area_name +='<option value="'+data.data[i].ids+'" >'+ data.data[i].val+'</option>';
+					
+				}
+				document.getElementById(actionid).innerHTML = area_name;
+				},
+				error: function(xhr,type,errorThrown){
+				}
+				});
+				
 	},
 	//获取房间信息
 	getRoomInfo: function getRoomInfo() {
